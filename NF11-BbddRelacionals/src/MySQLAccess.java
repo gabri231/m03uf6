9c8bb899f7c3881023f32b278e867a28d4465038
Gabriel Calle Torrez <gabriel.calle92@gmail.com>
2015-12-02 19:22:10 +0100

    Metodo insertarRegistro() añadido.

--- NF11-BbddRelacionals/src/MySQLAccess.java
+++ NF11-BbddRelacionals/src/MySQLAccess.java
@@ -1,9 +1,12 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
+
+
 
 public class MySQLAccess {
   private Connection connect = null;
@@ -12,21 +15,34 @@ public class MySQLAccess {
 
   public void readDataBase() throws Exception {
     try {
-      // This will load the MySQL driver, each DB has its own driver
-      Class.forName("com.mysql.jdbc.Driver");
-      // Setup the connection with the DB
-      connect = DriverManager
-          .getConnection("jdbc:mysql://localhost/test?"
-              + "user=gabriel&password=gabriel");
+    	String driver   = "com.mysql.jdbc.Driver";
+        String url      = "jdbc:mysql://localhost/test";
+        String usuario  = "gabriel";
+        String password = "gabriel";
 
-      // Statements allow to issue SQL queries to the database
-      statement = connect.createStatement();
-      // Result set get the result of the SQL query
-      resultSet = statement
-          .executeQuery("select * from test.comments");
-      writeResultSet(resultSet);
+		// Se carga el driver de conexión de MySql
+        Class.forName(driver);
+		// Se define la conexión con la base de datos
+		connect = DriverManager.getConnection(url, usuario, password); 
+				//DriverManager.getConnection("jdbc:mysql://localhost/test?user=gabriel&password=gabriel");
+		
+		// Statements allow to issue SQL queries to the database
+		statement = connect.createStatement();
 
-
+		//Datos a pasar por el metodo
+		String user    = "gabriel";
+		String mail    = "gabriel.calle92@gmail.com";
+		String web     = "www.escoladeltreball.org";
+		String summary = "bla bla bla";
+		String comment = "Comentarios del sitio";
+		
+		// Metodo para insertar un registro
+		insertarRegistro(user, mail, web, summary, comment);
+		
+		// Result set get the result of the SQL query
+		resultSet = statement.executeQuery("select * from test.comments");
+		writeResultSet(resultSet);
+		
     } catch (Exception e) {
       throw e;
     } finally {
@@ -34,13 +50,41 @@ public class MySQLAccess {
     }
 
   }
-
  
+  /* FUNCION PARA INSERTAR UN REGISTRO EN UNA BASE DE DATOS.
+   * 
+   * @param user	String
+   * @param mail	String
+   * @param web		String
+   * @param summary	String
+   * @param comment	String
+   */
+  private void insertarRegistro(String user, String mail, String web, String summary, String comment){
+		String query = "insert into test.comments (myuser, email, webpage ,summary, datum, comments) values (?,?,?,?,?,?)";
+		try{
+			Date hoy = new java.util.Date();
+			PreparedStatement statement = connect.prepareStatement(query);
+			statement.setString(1, user);
+			statement.setString(2, mail);
+			statement.setString(3, web);
+			statement.setString(4, summary);
+			statement.setDate(5, new java.sql.Date(hoy.getTime()));
+			statement.setString(6, comment);
+			
+			if (statement.executeUpdate() == 1){
+				System.out.println("Se ha insertado el registro\n");	
+			}
+		}catch (SQLException e){
+			System.err.println("ERROR: SQLException");
+			e.printStackTrace();
+		}
+	}
+  
   private void writeResultSet(ResultSet resultSet) throws SQLException {
     // ResultSet is initially before the first data set
     while (resultSet.next()) {
       // It is possible to get the columns via name
-	// also possible to get the columns via the column number
+      // also possible to get the columns via the column number
       // which starts at 1
       // e.g. resultSet.getSTring(2);
       String user = resultSet.getString("myuser");
@@ -53,6 +97,7 @@ public class MySQLAccess {
       System.out.println("Summary: " + summary);
       System.out.println("Date: " + date);
       System.out.println("Comment: " + comment);
+      System.out.println("---------------------------");
     }
   }
 
